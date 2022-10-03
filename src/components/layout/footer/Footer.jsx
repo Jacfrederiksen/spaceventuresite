f453f0e5ceea8a7725b0b013f0640ebb9a551b9c
Jacob Frederiksen <jac.frederiksen@gmail.com>
2022-10-03 19:40:40 +0200

    Mere footer

--- src/components/layout/footer/Footer.jsx
+++ src/components/layout/footer/Footer.jsx
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react'
 import styles from "./footer.module.scss"
 import { getFooter } from '../../../helpers/getCall';
+import { FaPhoneAlt, } from 'react-icons/fa'
+import { HiMail } from 'react-icons/hi'
+import { TiLocationArrow } from 'react-icons/ti'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
 
@@ -31,8 +35,34 @@ const Footer = () => {
       { loading && <p>Loader</p> }
       { error && <p>Error</p>}
       { footer && <div className={styles.footer_top}>
-        <div className={styles.contact_con}></div>
-        <div className={styles.links_con}></div>
+        <div className={styles.contact_con}>
+          <h3>KONTAKT</h3>
+          <div>
+            <FaPhoneAlt size="20px"/>
+            <p>{footer.phone}</p>
+          </div>
+          <div>
+            <HiMail size="25px"/>
+            <p>{footer.email}</p>
+          </div>
+          <div>
+            <TiLocationArrow size="30px"/>
+            <p>{footer.address}</p>
+          </div>
+        </div>
+        <div className={styles.links_con}>
+          <h3>HURTIG LINKS</h3>
+          <nav>
+            <ul>
+              <Link><li>Rumfærgen</li></Link>
+              <Link><li>Ture</li></Link>
+              <Link><li>Vores Team</li></Link>
+              <Link><li>Galleri</li></Link>
+              <Link><li>Sikkerhed</li></Link>
+            </ul>
+          </nav>
+          <button>Kontakt</button>
+        </div>
       </div>
       }
       <div className={styles.footer_bot}>
